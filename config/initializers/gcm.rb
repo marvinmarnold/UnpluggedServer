f8f192725b2d2d2434a9196ba31a4783354abbdd
Marvin Arnold <marvinmarnold@gmail.com>
2015-02-11 16:37:44 +0100

    Problem: No GCM gem

---
+++ config/initializers/gcm.rb
@@ -0,0 +1,7 @@
+GCM = GCM.new("AIzaSyCnr3U_YFk9GsOdsfHnOfoyAkAzqHW0WdU")
+# you can set option parameters in here
+#  - all options are pass to HTTParty method arguments
+#  - ref: https://github.com/jnunemaker/httparty/blob/master/lib/httparty.rb#L40-L68
+#  gcm = GCM.new("my_api_key", timeout: 3)
+
+registration_ids= ["268905631239"] # an array of one or more client registration IDs
